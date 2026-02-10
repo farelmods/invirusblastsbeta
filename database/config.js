04cc19dee9e439bcd52d68dc9b163965b6898d81
farelmods <farelmarwa14@gmail.com>
2026-02-10 17:41:12 +0800

    Add files via upload

---
+++ database/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  tokens: "8530367914:AAGY-fGpFpLw1uWOIm4xI5w5xw-40WLc1vM",  // Ubah Jadi Token Bot Mu !!!
+  owner: "7890825241", // Ubah Jadi Id Mu !!!
+  port: "4384", // Ubah Jadi Port Panel Mu !!!
+  ipvps: "178.128.121.139" // Ubah Jadi Ip Vps Mu !!!
+};
